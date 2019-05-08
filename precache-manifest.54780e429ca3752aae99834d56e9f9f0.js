9c7fcac37dbc0cc812007dd7e9183ea1fc0281cb
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-05-08 19:57:58 +0300

    Updates

---
+++ precache-manifest.54780e429ca3752aae99834d56e9f9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c65b2b4d4ffcd55d783503030883bd",
+    "url": "/squash/index.html"
+  },
+  {
+    "revision": "966bfe721fd02778f145",
+    "url": "/squash/static/css/main.ad87012e.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/squash/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "966bfe721fd02778f145",
+    "url": "/squash/static/js/main.528aa561.chunk.js"
+  },
+  {
+    "revision": "792a261a8f1d54d1ce6e",
+    "url": "/squash/static/js/runtime~main.7d8d6123.js"
+  }
+]);
