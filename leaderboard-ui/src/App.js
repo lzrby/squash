3acf7228adbab0454bc9bd7e2524935483e7dae8
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-05-08 19:53:06 +0300

    [ui]: add simple leaderboard

--- leaderboard-ui/src/App.js
+++ leaderboard-ui/src/App.js
@@ -1,6 +1,39 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import data from './raiting.json';
+import { DEFAULT_AVATAR, CUP_LOGO } from './constants';
 
-const App = () => <div>Hello</div>;
+const Player = ({ name, image, rating }) => (
+  <div className="list__person">
+    <img className="person__image" src={image} alt={name} />
+    <p className="person__name">{name}</p>
+    <p className="person__rating">{rating}</p>
+  </div>
+);
+
+Player.defaultProps = {
+  image: DEFAULT_AVATAR,
+};
+
+const Header = () => (
+  <div className="header">
+    <img className="header__icon" src={CUP_LOGO} alt="logo" />
+    <h1 className="header__title">
+      Squash
+      <span>Leaderboard</span>
+    </h1>
+  </div>
+);
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <div className="list">
+      {data.map(player => (
+        <Player key={player.name} {...player} />
+      ))}
+    </div>
+  </div>
+);
 
 export default App;
---
+++ leaderboard-ui/src/constants.js
@@ -0,0 +1,5 @@
+export const DEFAULT_AVATAR =
+  'https://iupac.org/wp-content/uploads/2018/05/default-avatar.png';
+
+export const CUP_LOGO =
+  'https://user-images.githubusercontent.com/23297041/55285200-a24e9b00-538f-11e9-8990-d49a162824d1.png';
