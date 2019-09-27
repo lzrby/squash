a92f4601b222a7a1144e13f0136925fb3bbd4482
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-27 17:09:00 +0300

    bot: swith to telebot

--- bot/settings.py
+++ bot/settings.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv('TG_TOKEN')
+groups = [-325409771]
+admins = ['drapegnik']
---
+++ bot/utils.py
@@ -0,0 +1,2 @@
+def format_tags(usernames):
+    return ', '.join([f'@{u}' for u in usernames])
