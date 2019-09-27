a010139beb8102ba079c19efd65060b15afaecb7
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-28 00:11:59 +0300

    bot: change game input format

--- bot/utils.py
+++ bot/utils.py
@@ -1,7 +1,7 @@
 import re
 
-# https://regex101.com/r/9dIo49/1
-GAME_REGEX = r'/game @([^\s]+) (\d+)-(\d+) @([^\s]+)'
+# https://regex101.com/r/9dIo49/2
+GAME_REGEX = r'/game @([^\s]+) (\d+):(\d+) @([^\s]+)'
 
 def format_tags(usernames):
     return ', '.join([f'@{u}' for u in usernames])
