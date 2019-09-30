3cfd1a655918f7d7939d0388f5aff7bfe5d91aba
wirbot <wir.development@gmail.com>
2019-09-30 15:06:05 +0000

    Updates

--- precache-manifest.ca4cbcdaebe73363fe1eec24a079ccfa.js
+++ precache-manifest.ca4cbcdaebe73363fe1eec24a079ccfa.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "491b991c9ee45d92d5edc5744781d7e8",
+    "revision": "c0e5d843bf74ebf373b8675e76055187",
     "url": "/squash/index.html"
   },
   {
-    "revision": "380a61045ca08fdc4efb",
+    "revision": "f80477bb68d74b3a315c",
     "url": "/squash/static/css/main.8ccecffa.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/squash/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "380a61045ca08fdc4efb",
-    "url": "/squash/static/js/main.0fc8ccc9.chunk.js"
+    "revision": "f80477bb68d74b3a315c",
+    "url": "/squash/static/js/main.fa6b83f1.chunk.js"
   },
   {
     "revision": "792a261a8f1d54d1ce6e",
